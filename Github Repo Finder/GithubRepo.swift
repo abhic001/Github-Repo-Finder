9093c2be52951f2f4376e725c421788413d3e2cf
Abhijeet Chakrabarti <abchakra10@gmail.com>
2017-03-07 06:42:30 -0500

    Updated the code

--- Github Repo Finder/GithubRepo.swift
+++ Github Repo Finder/GithubRepo.swift
@@ -15,7 +15,7 @@ private let clientSecret: String? = nil
 
 // Model class that represents a GitHub repository
 class GithubRepo: CustomStringConvertible {
-
+    
     var name: String?
     var ownerHandle: String?
     var ownerAvatarURL: String?
@@ -56,7 +56,7 @@ class GithubRepo: CustomStringConvertible {
     class func fetchRepos(_ settings: GithubRepoSearchSettings, successCallback: @escaping ([GithubRepo]) -> (), error: ((Error?) -> ())?) {
         let manager = AFHTTPRequestOperationManager()
         let params = queryParamsWithSettings(settings)
-
+        
         manager.get(reposUrl, parameters: params, success: { (operation: AFHTTPRequestOperation, responseObject: Any) in
             if let response = responseObject as? NSDictionary, let results = response["items"] as? NSArray {
                 var repos: [GithubRepo] = []
@@ -96,7 +96,7 @@ class GithubRepo: CustomStringConvertible {
         
         return params
     }
-
+    
     // Creates a text representation of a GitHub repo
     var description: String {
         return "[Name: \(self.name!)]" +
@@ -104,6 +104,6 @@ class GithubRepo: CustomStringConvertible {
             "\n\t[Forks: \(self.forks!)]" +
             "\n\t[Owner: \(self.ownerHandle!)]" +
             "\n\t[Avatar: \(self.ownerAvatarURL!)]" +
-            "\n\t[Description: \(self.repoDescription!)]"
+        "\n\t[Description: \(self.repoDescription!)]"
     }
 }
